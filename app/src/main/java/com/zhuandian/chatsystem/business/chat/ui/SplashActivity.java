6ffb1c71dbf7bc8669c42b918844809e0b107c16
xiedong <xiedong11@aliyun.com>
2019-05-10 17:52:42 +0800

    移植IM功能

---
+++ app/src/main/java/com/zhuandian/chatsystem/business/chat/ui/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.zhuandian.chatsystem.business.chat.ui;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.zhuandian.chatsystem.R;
+import com.zhuandian.chatsystem.business.chat.base.BaseActivity;
+import com.zhuandian.chatsystem.business.chat.bean.User;
+import com.zhuandian.chatsystem.business.chat.model.UserModel;
+
+
+/**启动界面
+ * @author :smile
+ * @project:SplashActivity
+ * @date :2016-01-15-18:23
+ */
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_im);
+        Handler handler =new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                User user = UserModel.getInstance().getCurrentUser();
+                if (user == null) {
+                    startActivity(LoginActivity.class,null,true);
+                }else{
+                    startActivity(MainActivity.class,null,true);
+                }
+            }
+        },1000);
+
+    }
+}
